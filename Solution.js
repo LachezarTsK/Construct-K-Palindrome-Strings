4514a7cdf42b16dd0dbd66922f6cdbc58cf8cab2
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-01-11 11:00:49 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param {string} input
+ * @param {number} targetNumberOfPalindromes
+ * @return {boolean}
+ */
+var canConstruct = function (input, targetNumberOfPalindromes) {
+    if (input.length <= targetNumberOfPalindromes) {
+        return input.length === targetNumberOfPalindromes;
+    }
+    const ASCII_SMALL_CASE_A = 97;
+
+    let bitStampForNumberOfOccurencesOfLettersWithOddFrequency = 0;
+    for (let i = 0; i < input.length; ++i) {
+        bitStampForNumberOfOccurencesOfLettersWithOddFrequency ^= 1 << (input.codePointAt(i) - ASCII_SMALL_CASE_A);
+    }
+
+    let numberOfOccurencesOfLettersWithOddFrequency = 0;
+    while (bitStampForNumberOfOccurencesOfLettersWithOddFrequency > 0) {
+        numberOfOccurencesOfLettersWithOddFrequency += bitStampForNumberOfOccurencesOfLettersWithOddFrequency & 1;
+        bitStampForNumberOfOccurencesOfLettersWithOddFrequency >>= 1;
+    }
+
+    return numberOfOccurencesOfLettersWithOddFrequency <= targetNumberOfPalindromes;
+};
