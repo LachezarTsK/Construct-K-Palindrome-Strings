b85b7f68a2fe1f1854dc95e3a117834f8cfaf2c2
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-01-11 11:01:58 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,26 @@
+
+#include <string>
+using namespace std;
+
+class Solution {
+
+public:
+    bool canConstruct(string input, int targetNumberOfPalindromes) const {
+        if (input.length() <= targetNumberOfPalindromes) {
+            return input.length() === targetNumberOfPalindromes;
+        }
+
+        int bitStampForNumberOfOccurencesOfLettersWithOddFrequency = 0;
+        for (const auto& letter : input) {
+            bitStampForNumberOfOccurencesOfLettersWithOddFrequency ^= 1 << (letter - 'a');
+        }
+
+        int numberOfOccurencesOfLettersWithOddFrequency = 0;
+        while (bitStampForNumberOfOccurencesOfLettersWithOddFrequency > 0) {
+            numberOfOccurencesOfLettersWithOddFrequency += bitStampForNumberOfOccurencesOfLettersWithOddFrequency & 1;
+            bitStampForNumberOfOccurencesOfLettersWithOddFrequency >>= 1;
+        }
+
+        return numberOfOccurencesOfLettersWithOddFrequency <= targetNumberOfPalindromes;
+    }
+};
